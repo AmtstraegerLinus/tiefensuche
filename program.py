d0e0bc063ef7334893cff7ea29d0e59046c510ec
AmtstraegerLinus <hoh230221@spengergasse.at>
2026-03-30 16:56:17 +0200

    Added default for geuschter_knoten

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 matrix = [
-#   A  B  C  D  E  F
+    #   A  B  C  D  E  F
     [0, 1, 1, 0, 0, 0],  # A (0)
     [0, 0, 0, 1, 1, 0],  # B (1)
     [0, 0, 0, 0, 0, 1],  # C (2)
@@ -11,14 +11,15 @@ matrix = [
 namen = ["A", "B", "C", "D", "E", "F"]
 knoten = []
 
-def dfs(matrix, start_knoten, gesuchter_knoten, besucht=None):
+
+def dfs(matrix, start_knoten, gesuchter_knoten=-1, besucht=None):
     if besucht is None:
         besucht = set()
 
     besucht.add(start_knoten)
     knoten.append(namen[start_knoten])
 
-    #print(start_knoten, gesuchter_knoten)
+    # print(start_knoten, gesuchter_knoten)
     if start_knoten == gesuchter_knoten:
         print("Knoten", namen[start_knoten], "wurde im Baum gefunden")
         return
@@ -28,7 +29,7 @@ def dfs(matrix, start_knoten, gesuchter_knoten, besucht=None):
             dfs(matrix, nachbar, gesuchter_knoten, besucht)
 
 
-dfs(matrix, 0, 3)
+dfs(matrix, 0)
 # Ausgabe: A B D E C F
 
 print(knoten)
