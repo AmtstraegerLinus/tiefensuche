d0b6a0ca436f664e5344ce00a7df1e0dcd00adbe
AmtstraegerLinus <hoh230221@spengergasse.at>
2026-03-30 16:23:50 +0200

    corrected shell.nix for locking one python version

--- shell.nix
+++ shell.nix
@@ -1,11 +1,20 @@
+# shell.nix
 let
-  pkgs = import <nixpkgs> { };
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1073dad219cb244572b74da2b20c7fe39cb3fa9e.tar.gz")
+      { };
 in
 pkgs.mkShell {
   packages = [
-    (pkgs.python3.withPackages (python-pkgs: [
-      python-pkgs.pandas
-      python-pkgs.requests
-    ]))
+    (pkgs.python3.withPackages (
+      python-pkgs: with python-pkgs; [
+        # select Python packages here
+        pandas
+        requests
+      ]
+    ))
   ];
 }
